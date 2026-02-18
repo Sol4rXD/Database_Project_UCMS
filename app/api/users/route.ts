6758f1afe53e206a9a5b1663bf7fdfa464360a6b
Watcharawish Onsanit <watcharawish@Watcharawishs-MacBook-Pro.local>
2026-02-18 23:00:29 +0700

    feat: add POST function api

---
+++ app/api/users/route.ts
@@ -0,0 +1,32 @@
+import { prisma } from "@/lib/prisma";
+
+export async function POST(req: Request) {
+  try {
+    const {
+      username,
+      password,
+      fullname,
+      surname,
+      student_id,
+      faculty,
+      department
+    } = await req.json()
+
+    const user = await prisma.users.create({
+      data: {
+        username,    
+        password,    
+        fullname,
+        surname,
+        student_id,
+        faculty,
+        department
+      }
+    })
+    return Response.json(user)
+  } catch (error) {
+    return new Response(error as BodyInit, {
+      status: 500
+    })
+  }
+}
