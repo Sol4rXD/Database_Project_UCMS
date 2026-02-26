09322a58abb0d61182b3a80b9f2a5e8c06a3289f
Watcharawish Onsanit <watcharawish@Watcharawishs-MacBook-Pro.local>
2026-02-26 14:37:50 +0700

    feat: add club api method by slug

---
+++ app/api/club/[slug]/route.ts
@@ -0,0 +1,96 @@
+import { connectDB } from "@/lib/mongodb";
+import { Club } from "@/models/Club";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  req: Request,
+  { params }: { params: { slug: string } }
+) {
+  try {
+    await connectDB();
+    const { slug } = await params;
+    console.log("Slug ที่รับมาคือ:", slug);
+    const club = await Club.findOne({ slug: slug }); 
+
+    if (!club) {
+      return NextResponse.json(
+        { error: "Club not found" }, 
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(club);
+  } catch (error) {
+    console.error("GET by Slug Error:", error);
+    return NextResponse.json(
+      { error: "Error to fetch data" }, 
+      { status: 500 }
+    );
+  }
+}
+
+export async function PUT(
+  req: Request,
+  { params }: { params: { slug: string } }
+) {
+  try {
+    await connectDB();
+    const { slug } = await params;
+    const body = await req.json();
+
+    const updatedClub = await Club.findOneAndUpdate(
+      { slug },
+      body,
+      { new: true }
+    );
+
+    if (!updatedClub) {
+      return NextResponse.json(
+        { error: "Club not found" },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(
+      { message: "Update club success", data: updatedClub },
+      { status: 200 }
+    );
+  } catch (error: any) {
+    console.error("PUT by Slug Error:", error);
+    return NextResponse.json(
+      { error: error.message ?? "Error to update club" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { slug: string } }
+) {
+  try {
+    await connectDB();
+    const { slug } = await params;
+
+    const deleted = await Club.findOneAndDelete({ slug });
+
+    if (!deleted) {
+      return NextResponse.json(
+        { error: "Club not found" },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(
+      { message: "Delete club success" },
+      { status: 200 }
+    );
+  } catch (error: any) {
+    console.error("DELETE by Slug Error:", error);
+    return NextResponse.json(
+      { error: error.message ?? "Error to delete club" },
+      { status: 500 }
+    );
+  }
+}
+
